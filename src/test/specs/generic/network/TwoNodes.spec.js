2cecbdd8f83619e65920af27f61caab6fc87d8c8
Jeffrey Esquivel S <jeff.esquivel@gmail.com>
2018-02-13 14:11:22 -0600

    Initial MockWebRtc Support

--- src/test/specs/generic/network/TwoNodes.spec.js
+++ src/test/specs/generic/network/TwoNodes.spec.js
@@ -7,28 +7,60 @@ describe('TwoNodes', () => {
         MockNetwork.uninstall();
     });
 
-    it('should be able to connect and reach consensus', (done) => {
-        (async () => {
-            let established1, established2;
+    it('should be able to connect and reach consensus through WebSocket', (done) => {
+        let established = false;
 
+        function checkEstablished(){
+            if (established) done();
+            established = true;
+        }
+
+        (async () => {
             const netconfig = new WsNetworkConfig('node1.test', 9000, 'key1', 'cert1');
             const consensus1 = await Consensus.volatileFull(netconfig);
-            consensus1.on('established', () => {
-                if (established2) done();
-                established1 = true;
-            });
+            consensus1.on('established', checkEstablished);
 
             PeerAddresses.SEED_PEERS = [WsPeerAddress.seed('node1.test', 9000)];
 
             const consensus2 = await Consensus.volatileFull();
-            consensus2.on('established', () => {
-                if (established1) done();
-                established2 = true;
-            });
+            consensus2.on('established', checkEstablished);
 
             consensus2.network.connect();
 
             expect(true).toBe(true);
+        })().catch(done.fail);
+    });
+
+    it('should be able to connect and sync through WebRTC', (done) => {
+        let consensus3, established = false;
+
+        function checkEstablished() {
+            if (established) {
+                expect(consensus3._agents.length).toBe(2);
+                done();
+            }
+            established = true;
+        }
+
+        (async () => {
+            const netconfig = new WsNetworkConfig('node1.test', 9000, 'key1', 'cert1');
+            const consensus1 = await Consensus.volatileFull(netconfig);
+            consensus1.on('established', checkEstablished);
+
+            PeerAddresses.SEED_PEERS = [WsPeerAddress.seed('node1.test', 9000)];
+
+            spyOn(PlatformUtils, 'supportsWebRTC').and.callFake(() => {
+                return true;
+            });
+
+            const consensus2 = await Consensus.volatileFull();
+            consensus2.network.connect();
+
+            consensus2.on('established', async () => {
+                consensus3 = await Consensus.volatileLight();
+                consensus3.network.connect();
+                consensus3.on('established', checkEstablished);
+            });
         })().catch(done.fail);
     });
 });
